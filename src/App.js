2ffd9ff39dc0ede58e4d8644046abd18eb5877b6
gedunlap <g.e.dunlap90@gmail.com>
2021-12-06 10:55:00 -0500

    somestuff

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <img className="logo" src="https://i.imgur.com/bnKEpr6.jpg" alt="Material Tampa" />
-      <p className="text">Our site is currently under construction. Please make and appointment and check us out here later.</p>
-      <img className="x" src="https://i.imgur.com/o2Q2xAa.png" alt="Scissors" />
+      <div className="container">
+        <img className="logo" src="https://i.imgur.com/t3cbqzd.png" alt="Material Tampa" />
+        <p className="text1">Our site is currently under construction.</p>
+        <p className="text2">Check us out here later.</p>
+        <img className="x" src="https://i.imgur.com/o2Q2xAa.png" alt="Scissors" />
+      </div>
     </div>
   );
 }
