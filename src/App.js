53ffcaeae9a6d454f6ee59771ef2a6a3d5a49462
gedunlap <g.e.dunlap90@gmail.com>
2021-12-08 12:01:22 -0500

    uncomment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <div className="container">
         <img className="logo" src="https://i.imgur.com/t3cbqzd.png" alt="Material Tampa" />
         <p className="text1">Our site is currently under construction</p>
-        {/* <a href="" target="_blank"><button>Book An Appointment</button></a> */}
+        <a href="" target="_blank" rel="noreferrer"><button>Book An Appointment</button></a>
         <p className="text2">Check us out on Instagram</p>
         <a href="https://www.instagram.com/material.tpa/" target="_blank" rel="noreferrer"><img className="ig" src="https://i.imgur.com/w87mdKt.png" alt="instagram logo" /></a>
         <img className="x" src="https://i.imgur.com/o2Q2xAa.png" alt="Scissors" />
