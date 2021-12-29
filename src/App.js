a206139b84319ee12e8b2d88ae602174c212e663
gedunlap <g.e.dunlap90@gmail.com>
2021-12-29 10:56:59 -0500

    external link appointments

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
       <div className="container">
         <img className="logo" src="https://i.imgur.com/EsCu0Sw.png" alt="Material Tampa" />
         <p className="text1">Our site is currently under construction</p>
-        {/* <a href="" target="_blank" rel="noreferrer"> */}
+        <a href="https://www.fresha.com/a/material-tampa-2502-north-howard-avenue-tjr5uxjo/booking?menu=true&dppub=true" target="_blank" rel="noreferrer">
           <button>Book An Appointment</button>
-        {/* </a> */}
+        </a>
         <p className="text2">Check us out on Instagram</p>
         <a href="https://www.instagram.com/material.tpa/" target="_blank" rel="noreferrer"><img className="ig" src="https://i.imgur.com/w87mdKt.png" alt="instagram logo" /></a>
         <img className="m" src="https://i.imgur.com/w6tXxGd.jpg" alt="Scissors" />
