40045e75c1ba4750dd3df3bc4fd83040c34bd95e
gedunlap <g.e.dunlap90@gmail.com>
2022-06-23 19:19:04 -0400

    bye bye contruct

--- src/App.js
+++ src/App.js
@@ -3,22 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <img className="logo" src="https://i.imgur.com/EsCu0Sw.png" alt="Material Tampa" />
-        <p className="text1">Our site is currently under construction</p>
-        <a href="https://www.fresha.com/a/material-tampa-2502-north-howard-avenue-tjr5uxjo/booking?menu=true&dppub=true" target="_blank" rel="noreferrer">
-          <button>Book An Appointment</button>
-        </a>
-        <p className="text2">Check us out on Instagram</p>
-        <a href="https://www.instagram.com/materialtampa/" target="_blank" rel="noreferrer"><img className="ig" src="https://i.imgur.com/w87mdKt.png" alt="instagram logo" /></a>
-        <p>Contact us by email</p>
-        <a href="mailto: contact@materialtampa.com" target="_blank" rel="noreferrer"><img className='email' src="https://i.imgur.com/CtrafJJ.png" alt="email-icon" /></a>
-        <img className="m" src="https://i.imgur.com/w6tXxGd.jpg" alt="logo" />
-        <p className='address'>
-          2502 N. Howard Ave Suite B 
-          <br />
-          Tampa, FL 33607
-        </p>
+      <div className='textpage'>
+        <img className='textlogo' src="https://i.imgur.com/Yan6SO9.jpg" alt="logo" />
+        <h1><b>Price Adjustments</b></h1>
+        <p>We are here to offer you the utmost service quality and experience. In order to continue giving you our best, you may see some price adjustments starting July 1.
+        <br/>
+        <br/>
+        Due to inflation and the rising cost of goods there will be a 5-10% increase in some services. Please check the website at <a href="https://www.materialtampa.com/">www.materialtampa.com</a> for updated pricing.
+        <br/>
+        <br/>
+        We thank you all for your continued support and loyalty.
+        If you have any questions please email us at <a href="mailto:contact@materialtampa.com">contact@materialtampa.com</a></p>
       </div>
     </div>
   );
