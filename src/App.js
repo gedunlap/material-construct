beb576f7a0090601ce4fef1486e89e451804b4ac
gedunlap <g.e.dunlap90@gmail.com>
2022-12-28 08:55:59 -0500

    new year update

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ function App() {
       <div className='textpage'>
         <img className='textlogo' src="https://i.imgur.com/Yan6SO9.jpg" alt="logo" />
         <h1><b>Price Adjustments</b></h1>
-        <p>We are here to offer you the utmost service quality and experience. In order to continue giving you our best, you may see some price adjustments starting July 1.
+        <p>Thank you to everyone for an amazing 2022! We truly appreciate the love, loyalty and support you all have shown us throughout the year. 
         <br/>
         <br/>
-        Due to inflation and the rising cost of goods there will be a 5-10% increase in some services. Please check the website at <a href="https://www.materialtampa.com/">www.materialtampa.com</a> for updated pricing.
+        Starting January 1st we will only be taking tips and gratuity in the form of cash or Venmo. You will no longer be able to add a tip or gratuity through our POS system. Please plan accordingly.
+        <br />
+        <br />
+        You may also see a small 5-10% increase in SOME services. Please check the website at <a href="https://www.materialtampa.com/">www.materialtampa.com</a> for updated pricing.
         <br/>
         <br/>
-        We thank you all for your continued support and loyalty.
         If you have any questions please email us at <a href="mailto:contact@materialtampa.com">contact@materialtampa.com</a></p>
       </div>
     </div>
