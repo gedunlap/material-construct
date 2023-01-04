fc76a369dd10e746606b3dc538b2ce385f228bcd
gedunlap <g.e.dunlap90@gmail.com>
2023-01-04 11:57:20 -0500

    party invite

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import myGif from './Invite.gif';
 
 function App() {
   return (
     <div className="App">
       <div className='textpage'>
-        <img className='textlogo' src="https://i.imgur.com/Yan6SO9.jpg" alt="logo" />
+        {/* <img className='textlogo' src="https://i.imgur.com/Yan6SO9.jpg" alt="logo" />
         <h2><b>New Year Update</b></h2>
         <p>Thank you to everyone for an amazing 2022! We truly appreciate the love, loyalty and support you all have shown us throughout the year. 
         <br/>
@@ -15,7 +16,9 @@ function App() {
         You may also see a small 5-10% increase in SOME services. Please check the website at <a href="https://www.materialtampa.com/">www.materialtampa.com</a> for updated pricing.
         <br/>
         <br/>
-        If you have any questions please email us at <a href="mailto:contact@materialtampa.com">contact@materialtampa.com</a></p>
+        If you have any questions please email us at <a href="mailto:contact@materialtampa.com">contact@materialtampa.com</a></p> */}
+        <img className='gif' src={myGif} alt="partytime" />
+        {/* <button><a href="mailto:contact@materialtampa.com">RSVP</a></button> */}
       </div>
     </div>
   );
